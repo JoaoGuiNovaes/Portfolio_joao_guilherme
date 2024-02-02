8eaf4808efb68d2480cd4fa37d77721b5df70b96
Henry Carvalho <128334320+JoaoGuiNovaes@users.noreply.github.com>
2024-02-02 16:17:54 -0300

    Responsividade
    Responsividade, vários media queries, menu hamburguer, toggle de tudo, active da nacbar, bordinha no header

---
+++ script.js
@@ -0,0 +1,60 @@
+let hamburguer = document.getElementById('menu-icon');
+let navbar = document.getElementById('navbar');
+let links = document.querySelectorAll('.navoption');
+
+hamburguer.onclick = () =>{
+    hamburguer.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+        hamburguer.classList.toggle('bx-x');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            // Adiciona a classe 'active' a todos os links com o atributo href correspondente
+            document.querySelectorAll('header nav a[href="#' + id + '"]').forEach(link => {
+                link.classList.add('active');
+            });
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
